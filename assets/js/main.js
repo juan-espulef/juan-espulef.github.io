ab699c572efd012273fdca33685fe219bd4bc680
Juan E. Espulef <99534798+JkqzDev@users.noreply.github.com>
2023-05-06 19:44:11 -0300

    Github action pages

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// Make particles
+const width = () => {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+};
+
+if (width < 400)  {
+    $.firefly ({
+        minPixel: 1,
+        maxPixel: 2,
+        total: 20
+    });
+} else {
+    $.firefly ({
+        minPixel: 1,
+        maxPixel: 3,
+        total: 40
+    });
+}
+
+//////
